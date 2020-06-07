9f6bde870121d6a8f2b4eb53e8505bb2829cb15e
Admin <Admin@DESKTOP-4A8333F>
2020-06-07 23:07:09 +0300

    AddLibMvc

--- TRbooks.tests/UnitTest1.cs
+++ TRbooks.tests/UnitTest1.cs
@@ -1,14 +1,52 @@
 ﻿using System;
+using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TRbooks.Controllers;
+
+
 
 namespace TRbooks.tests
 {
     [TestClass]
-    public class UnitTest1
+    public class HomeControllerTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Index()
         {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void About()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.About() as ViewResult;
+
+            // Assert
+            Assert.AreEqual("Your application description page.", result.ViewBag.Message);
+        }
+
+        [TestMethod]
+        public void Contact()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Contact() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
         }
     }
 }

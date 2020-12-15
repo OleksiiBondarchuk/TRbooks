f7abcb790d7662aa0cf21d047e6a31ba47808a22
Олексій Бондарчук <alexalex60421@gmail.com>
2020-12-15 16:34:10 +0200

    lit css and genre

--- Migrations/202005251934303_AddValuesToMovieGenre.cs
+++ Migrations/202005251934303_AddValuesToMovieGenre.cs
@@ -2,17 +2,21 @@
 {
     using System;
     using System.Data.Entity.Migrations;
-    
+
     public partial class AddValuesToMovieGenre : DbMigration
     {
         public override void Up()
         {
-            Sql("INSERT INTO MovieGenres (Name) VALUES ('Family')");
-            Sql("INSERT INTO MovieGenres (Name) VALUES ('Comedy')");
-            Sql("INSERT INTO MovieGenres (Name) VALUES ('Action')");
+            Sql("INSERT INTO MovieGenres (Name) VALUES ('Adventure')");
+            Sql("INSERT INTO MovieGenres (Name) VALUES ('Classics')");
+            Sql("INSERT INTO MovieGenres (Name) VALUES ('Graphic Novel')");
             Sql("INSERT INTO MovieGenres (Name) VALUES ('Horror')");
+            Sql("INSERT INTO MovieGenres (Name) VALUES ('Mystery')");
+            Sql("INSERT INTO MovieGenres (Name) VALUES ('Fantasy')");
+            Sql("INSERT INTO MovieGenres (Name) VALUES ('Romance')");
+
         }
-        
+
         public override void Down()
         {
         }
